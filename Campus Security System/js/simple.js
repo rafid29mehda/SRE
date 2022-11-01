280e7743f9925bf4c3b3782b2347b8d8477e06f6
Rafid Mehda <71279591+rafid29mehda@users.noreply.github.com>
2022-11-01 18:58:46 +0600

    Add files via upload

---
+++ Campus Security System/js/simple.js
@@ -0,0 +1,12 @@
+const toggleButton = document
+.querySelector('.toggle-button');
+
+const navbarLinks = document
+.querySelector('.navbar-links');
+
+toggleButton.addEventListener('click',
+    () => {
+        navbarLinks.classList.toggle('active');   
+    }
+
+);
